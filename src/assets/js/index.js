78127372621458fadd5e08cdac74d7d3450251cb
Alexander Morton <mortonprod@gmail.com>
2020-12-18 22:28:07 +0000

    Update FE call to use done

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -29,9 +29,11 @@ $('#form').submit(function(event) {
   $( "#form button" ).html( "<span class=\"spinner-grow spinner-grow-sm\" role=\"status\" aria-hidden=\"true\"></span>");
   $("#form button").prop('disabled', true);
   console.log(input.val());
-  $.get( `${TRANSFORM_DOMAIN}/?url=${input.val()}`, function( data ) {
+  $.get( `${TRANSFORM_DOMAIN}/?url=${input.val()}`)
+  .done( function(data ) {
     window.location.replace(`${window.location.hostname}/canvas?url=${data.body.url}`);
-  }).fail(function() {
+  })
+  .fail(function() {
     $( "#form button" ).html("");
     $("#form").append( "<div class=\"alert alert-danger alert-dismissible fade show\" role=\"alert\"> Something went wrong with that domain. Try another... <button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\"> <span aria-hidden=\"true\">&times;</span> </button> </div>");
     $( ".close" ).click(function() {
