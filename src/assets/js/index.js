0b75fa0d0cebddc74ee2b6f0a72188c7088c3951
Alexander Morton <mortonprod@gmail.com>
2020-12-18 21:39:49 +0000

    Include error code and canvas passing

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -2,7 +2,7 @@ import css from '../css/index.css';
 require('../../index.html');
 import image from '../images/suit.jpg';
 import video from '../images/clown.mp4';
-// TRANSFORM_DOMAIN="";
+var TRANSFORM_DOMAIN="https://vcxt1fzq20.execute-api.eu-west-2.amazonaws.com/dev";
 $(document).ready(function() {
   // var headerHeight = $('.navbar').outerHeight();
   $('#exploreButton a').click(function(e) {
@@ -23,7 +23,20 @@ $(document).ready(function() {
   });
 });
 
-$('form').submit(function() {
+$('#form').submit(function(event) {
+  event.preventDefault();
   var input = $('#form input');
+  $( "#form button" ).html( "<span class=\"spinner-grow spinner-grow-sm\" role=\"status\" aria-hidden=\"true\"></span>");
+  $("#form button").prop('disabled', true);
   console.log(input.val());
+  $.get( `${TRANSFORM_DOMAIN}/?url=${input.val()}`, function( data ) {
+    window.location.replace(`${window.location.hostname}/canvas?url=${data.body.url}`);
+  }).fail(function() {
+    $( "#form button" ).html("");
+    $("#form").append( "<div class=\"alert alert-danger alert-dismissible fade show\" role=\"alert\"> Something went wrong with that domain. Try another... <button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-label=\"Close\"> <span aria-hidden=\"true\">&times;</span> </button> </div>");
+    $( ".close" ).click(function() {
+      $( "#form > button" ).html( "Submit");
+      $("#form > button").prop('disabled', false);
+    })
+  });
 });
